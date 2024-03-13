712600cfcd4f37eb327e97e4c61b2e31df56636c
Mek0T0dadze <mtodadze2010@gmail.com>
2024-03-13 20:25:27 +0400

    commit

---
+++ Day 06/homework/lesson.6.2.py
@@ -0,0 +1,76 @@
+#1
+
+num1=int(input("enter num1: "))
+num2=float(input("enter num2: "))
+
+print(num1==num2)
+
+#2
+print(7 > 9)
+print(9 > 7)
+print(1000000 >10000)
+print(78 > 98)
+print(2 > 2)
+
+#3
+print(4 >= 4)
+print(44 >= 88)
+print(12345 >= 1234567890)
+print(100 >= 100)
+print(3 >= 3)
+
+#4
+print(9 < 0)
+print(22 < 11)
+print(44455566 < 777888999)
+print(9 < 9)
+print(1111111111 < 1111111111111)
+
+#5
+print(2 <= 2)
+print(456321 <= 1)
+print(4400 <= 453)
+print(73 <= 73)
+print(231 <= 9)
+
+#6
+print(9 == 9)
+print(72 == 82)
+print(99 == 99)
+print(6 == 6)
+print(0 == 0)
+
+#7
+print(7 != 7)
+print(32 != 31)
+print(60 != 50)
+print(43 != 45)
+print(3 != 3)
+
+#8
+num1=input("enter num1: ")
+num2=input("enter num2: ")
+
+print(num1<num2)
+print(num1<=num2)
+print(num1>num2)
+print(num1>=num2)
+print(num1==num2)
+print(num1!=num2)
+
+#9
+num1=int(input("enter num1"))
+print(num1>num1+10)
+print(num1>=num1+10)
+print(num1<num1+10)
+print(num1<=num1+10)
+print(num1==num1+10)
+print(num1!=num1+10)
+
+#10
+num1=int(input("enter num1: "))
+num2=float(input("enter num2: "))
+
+
+print(num1==num2)
+print(num1!=num2)
