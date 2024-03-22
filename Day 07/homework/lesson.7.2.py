8dd0b10be47fcf357699dc2cfb5b1480684c8ee7
Mek0T0dadze <mtodadze2010@gmail.com>
2024-03-22 21:02:32 +0400

    commit

---
+++ Day 07/homework/lesson.7.2.py
@@ -0,0 +1,32 @@
+#1
+temp=int(input("Enter what is the temperature today? "))
+
+print((temp*1.8)+32)
+
+#2
+age=int(input("Enter your age: "))
+print(age<20 and age>12)
+
+#3
+length=(float(input("Enter rectangles length: ")))
+width=(float(input("Enter rectangles width: ")))
+
+print(int(length*width))
+
+#4
+num1=(int(input("Enter num1: ")))
+
+print(int(num1>100 and num1//9))
+
+#5
+#(and)
+print(1>2 and 23<22)
+print(57>89 and 12>11)
+print(94>32 and 80>90)
+print(90==90 and 57<100)
+
+#(or)
+print(83==82 or 78>99)
+print(20<19 or 0<1)
+print(55>44 or 26<27)
+print(1==1 or 2==2)
