15bc692e50fbd3b574c98a6d8b0e8ae52bd4c713
Mek0T0dadze <mtodadze2010@gmail.com>
2024-04-05 20:28:29 +0400

    commit

---
+++ Day 09/homework/lesson.9.2.py
@@ -0,0 +1,8 @@
+salary=int(input("enter your salary: "))
+
+if salary==10000:
+    print("were you learning in GOA?")
+elif salary==1000 or 10000:
+    print("you are mid")
+else:
+    print("you should have entered GOA matrix")
