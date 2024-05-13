11f76452c5fb2962ba4afbe0fad8d3bf7dfe4193
Mek0T0dadze <mtodadze2010@gmail.com>
2024-05-14 01:09:21 +0400

    commit

---
+++ Day 17/classwork/lesson.17.py
@@ -0,0 +1,18 @@
+#1
+
+def ltext(list):
+    result=" " .join(list)
+    return list
+print(["i","am","spiderman"])
+
+#2
+def ltext(list):
+    result=" " .join(list)
+    return list
+print(["i","am","batman"])
+#join gamoiyeneba listshi elementebis chasamateblad
+
+def text(word,word1):
+    result=" ".split(text)
+    return text
+print("Meko","todadze")
---
+++ Day 17/homework/lesson.17.py
@@ -0,0 +1,10 @@
+#1
+#2
+def list(name,find_value):
+    for index in name:
+        if name[index]==find_value:
+            return index
+print(list["Meko","Luka","Mari"]["Mari"])
+#3
+#split() punqcia gamoiyeneba imistvis rm stringshi motavsebuli elementebi gaacalkevos
+#join() punqcia gamoiyenebastringshi mdebare elementebis shesaerteblad
